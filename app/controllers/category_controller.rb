9d941dfe68f01eef483fdab4b0701b376220561e
risa-1031 <mamina373.1031@gmail.com>
2020-08-07 02:45:43 +0900

    こっちがカテゴリ一覧できてる

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,2 +1,5 @@
 class CategoryController < ApplicationController
+  def index
+    @parents = Category.where(ancestry: nil)
+  end
 end
--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,5 +1,9 @@
 class ProductionsController < ApplicationController
+  # def index
+  #   @parents = Category.all.order("id ASC").limit(13)
+  # end
+
   def index
-    @parents = Category.all.order("id ASC").limit(13)
+    @parents = Category.where(ancestry: nil)
   end
 end
