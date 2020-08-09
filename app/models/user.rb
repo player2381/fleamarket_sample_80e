b62d78e6dc0fdf1ef102f08c3716cd2641cf31a1
risa-1031 <mamina373.1031@gmail.com>
2020-08-09 16:38:14 +0900

    ユーザー登録　誕生日登録できるまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
-
+  validates :birth_date, presence: true
   has_one :address
   has_one :card
   has_many :customers
