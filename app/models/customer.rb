5700ad13546568c09b72814b46f33a2c011412ed
player2381 <augic000@gmail.com>
2020-08-10 14:27:58 +0900

    デプロイ修正　valedation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,6 @@ class Customer < ApplicationRecord
   belongs_to :user
   # belongs_to :card
 
-  validates :user_id
+  #validates :user_id , presence: true
+  # validates :nickname , presence: true, uniqness: true, length: {maximun: 10}
 end
