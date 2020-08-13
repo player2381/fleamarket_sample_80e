f40d34c2b45e95000a108ba6a05a8d5c22fa3936
ken-ken-k <ken.5160soccer.4656@gmail.com>
2020-08-13 17:27:39 +0900

    ffix

--- app/models/production.rb
+++ app/models/production.rb
@@ -4,6 +4,5 @@ class Production < ApplicationRecord
   belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :customer
 
 end
