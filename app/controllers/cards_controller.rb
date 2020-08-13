2d57c1178e400a0cef70bd8eb54266d8f6bd92a9
risa-1031 <mamina373.1031@gmail.com>
2020-08-13 19:50:01 +0900

    コメントアウト修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,13 +4,13 @@ class CardsController < ApplicationController
   before_action :set_card
 
   def new
-    # card = Card.where(user_id: current_user.id).first
+    
     @card = Card.new
-    # redirect_to action: "new" if card.present?
+    
   end
 
   def create
-    # binding.pry
+    
     Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
     if params['payjp-token'].blank?
       
@@ -25,10 +25,10 @@ class CardsController < ApplicationController
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to user_path(current_user.id)
-        # flash[:notice] = 'クレジットカードの登録が完了しました'
+        
       else
         redirect_to action: "new"
-        # flash[:alert] = 'クレジットカード登録に失敗しました'
+        
       end
     end
   end
@@ -49,13 +49,6 @@ class CardsController < ApplicationController
     Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
     customer = Payjp::Customer.retrieve(@card.customer_id)
     @card = customer.cards.retrieve(@card.card_id)
-      
-  
-    # Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
-    # @card = Card.where(user_id: current_user.id).first
-    # customer = Payjp::Customer.retrieve(@card.customer_id)
-    # @card = customer.cards.retrieve(@card.card_id)
-    # @card = Card.where(user_id: current_user.id).first
   end
 
   private
--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -12,29 +12,7 @@ class ProductionsController < ApplicationController
   def create
   end
 
-  # def pay
-  #   Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
-  #   charge = Payjp::Charge.create(
-  #   amount: @production.price,
-  #   card: params['payjp-token'],
-  #   currency: 'jpy',
-  #   )
-  #   redirect_to root_path, notice: "支払いが完了しました"
-  # end
-
-  private
 
 
-  # def production_params
-  #   params.require(:production).permit(
-  #     :name,
-  #     :price,
-  #     :introduction,
-  #     :size,
-  #     :shipping_charge,
-  #     :prefecture_code,
-  #     :detail_date,
-  #     :trading_status,
-  #   ).merge(user_id: current_user.id)
-  # end
+  
 end
