42484b57ada9837a05a10736fa5f005a0cf2c946
risa-1031 <mamina373.1031@gmail.com>
2020-08-25 02:23:31 +0900

    リファクタリング

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,3 @@
 class Customer < ApplicationRecord
   belongs_to :user
-
-  #validates :user_id , presence: true
-  # validates :nickname , presence: true, uniqness: true, length: {maximun: 10}
 end
